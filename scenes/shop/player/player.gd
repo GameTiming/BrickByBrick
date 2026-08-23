3a240f6b976b976db6da962255fab4b052985ac1
Tausva <tautvydas.svarinskas@gmail.com>
2026-08-23 19:55:58 +0300

    player config to expor

--- scenes/shop/player/player.gd
+++ scenes/shop/player/player.gd
@@ -1,13 +1,12 @@
 class_name Player extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-@onready var camera: Camera3D = $Camera3D
+@export var speed: float = 5.0
+@export var jump_velocity: float = 4.5
+@export var mouse_sensitivity: float = 0.001
 
 var camera_rotation: Vector2 = Vector2.ZERO
-var mouse_sensitivity: float = 0.001
 
+@onready var camera: Camera3D = $Camera3D
 
 func _ready() -> void:
 	camera.current = true
@@ -38,15 +37,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
